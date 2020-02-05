228e6b06643474ef608553fd8d934b1e7668bf36
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 12:01:01 +0100

    Update User.js

--- src/structures/User.js
+++ src/structures/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const mongoose = require("mongoose");
@@ -13,19 +12,3 @@ const userShema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userShema);
-=======
-'use strict';
-
-const mongoose = require('mongoose');
-
-const userShema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    userID: String,
-    userName: String,
-    rep: Number,
-    serenity: Number,
-    guildData: Array
-});
-
-module.exports = mongoose.model('User', userShema);
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
