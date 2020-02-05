f5684bb46ae8122d626a4a14ab9350a146c0f581
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 12:02:19 +0100

    Update index.js

--- src/structures/index.js
+++ src/structures/index.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 "use strict";
 
 module.exports = {
   Guild: require("./guild"),
   User: require("./User")
 };
-=======
-'use strict';
-
-module.exports = {
-    Guild: require('./guild'),
-    User: require('./User'),
-};
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
