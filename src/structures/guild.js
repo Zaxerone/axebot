320e408d7b6d35ec912e25dd61d7556c7044c8ac
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 12:01:43 +0100

    Update guild.js

--- src/structures/guild.js
+++ src/structures/guild.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const mongoose = require("mongoose");
@@ -27,29 +26,3 @@ const guildShema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("Guild", guildShema);
-=======
-'use strict';
-
-const mongoose = require('mongoose');
-const { DEFAULTSETTINGS: defaults } = require('./../../config');
-
-const guildShema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    guildID: String,
-    guildName: String,
-    prefix: {
-        "type": String,
-        "default": defaults.prefix,
-    },
-    welcomeChannel: {
-        "type": String,
-        "default": defaults.welcomeChannel,
-    },
-    welcomeMessage: {
-        "type": String,
-        "default": defaults.welcomeMessage,
-    },
-});
-
-module.exports = mongoose.model('Guild', guildShema);
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
