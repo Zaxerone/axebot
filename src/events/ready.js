89f9b1a34e3f9e8def601be0a772874d3a8e3364
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 12:13:15 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const logger = require("./../lib/logger");
@@ -25,17 +24,3 @@ module.exports = async client => {
   logger.log(`${client.user.tag} is connected !`);
   await client.channels.get("668080342447423519").send(`${moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a')} - Bot démarré !`, {code: "c"});
 };
-=======
-'use strict'
-
-const logger = require('./../lib/logger');
-
-module.exports = async client => {
-    client.user.setPresence({ activity: { name: 'Anikami' }, status: 'idle' });
-    client.appInfo = await client.fetchApplication();
-    setTimeout(() => {
-        client.appInfo = client.fetchApplication();
-    }, 3600000);
-    logger.log(`${client.user.tag} est connecté !`);
-};
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
