81ebfdb9d2c5de0bedb06ff195e452e5c8073c50
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 13:58:56 +0100

    Update clear.js

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -25,20 +25,3 @@ module.exports = {
     });
   }
 };
-=======
-'use strict';
-
-module.exports = {
-    name: 'clear',
-    description: 'Supprime les messages',
-    aliases: ['purge'],
-    cooldown: 5,
-    execute(client, { args, channel, message }) {
-        channel.bulkDelete(args[0] + 1, true).then(msg => {
-            message.reply(`J'ai supprimé ${msg.size} message(s).`).then(mess => {
-                mess.delete({ timeout: 3000 });
-            });
-        });
-    },
-};
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
