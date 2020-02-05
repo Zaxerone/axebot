e7e3805b86c4a9d5979a2c1e21dffe5d2b5437da
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 11:56:17 +0100

    Update Buffer.js

--- src/Resolver/Buffer.js
+++ src/Resolver/Buffer.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 "use strict";
 
 const axios = require("axios");
@@ -10,16 +10,4 @@ module.exports = function buffer(data) {
     })
     .then(res => res.data)
     .catch(err => console.log(err));
-=======
-'use strict';
-
-const axios = require('axios');
-
-module.exports = function buffer(data) {
-    return axios.get(data, {
-            responseType: 'arraybuffer'
-        })
-        .then((res) => res.data)
-        .catch(err => console.log(err));
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
 };
