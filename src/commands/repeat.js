9da1f3c40c088ab007a58bce6cbc5aab03ba4c2a
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 14:08:20 +0100

    Update repeat.js

--- src/commands/repeat.js
+++ src/commands/repeat.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 "use strict";
-const { WebhookClient } = require('discord.js');
 
 module.exports = {
   name: "repeat",
@@ -15,18 +13,3 @@ module.exports = {
     .then(() => message.delete({ timeout: 0 }))
   }
 };
-=======
-'use strict';
-
-module.exports = {
-    name: 'repeat',
-    aliases: [],
-    description: 'Repete ce que l\'utilisateur dit',
-    guildOnly: true,
-    cooldown: 5,
-    execute(client, { message, args, channel }) {
-        channel.send(args.join(' '));
-        message.delete({ timeout: 3000 }).then(console.log('Un message a été supprimé !'));
-    }
-}
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
