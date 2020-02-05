05c08499143a4a1c867ceb09f8d1bf17355510fa
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 12:06:25 +0100

    Update functions.js

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-"use strict";
+<"use strict";
 
 const mongoose = require("mongoose");
 const { Guild, User } = require("./../structures");
@@ -54,55 +53,4 @@ module.exports = client => {
     await createUser.save().then(u => console.log(`New user -> ${u.userName}`));
     return false;
   };
-=======
-'use strict';
-
-const mongoose = require('mongoose');
-const { Guild, User } = require('./../structures');
-
-module.exports = client => {
-    client.getGuild = async guild => {
-        const data = await Guild.findOne({ guildID: guild.id });
-        if (data) return data;
-        return client.config.DEFAULTSETTINGS;
-    };
-
-    client.updateGuild = async (guild, settings) => {
-        let data = await client.getGuild(guild);
-        if (typeof data !== 'object') data = {};
-        for (const key in settings) {
-            if (data[key] !== settings[key]) data[key] = settings[key];
-        };
-        return data.updateOne(settings);
-    }
-
-    client.createGuild = async guild => {
-        const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, guild);
-        const createGuild = await new Guild(merged);
-        createGuild.save().then(g =>  console.log(`New guild -> ${g.guildName}`))
-    };
-
-    client.getUser = async user => {
-        const data = await User.findOne({ userID: user.id });
-        if (data) return data;
-        await client.createUser({ userID: user.id, userName: user.username, guildData: [], rep: 0, serenity: 100 });
-        return await User.findOne({ userID: user.id });
-    };
-
-    client.updateUser = async (user, settings) => {
-        let data = await client.getUser(user);
-        if (typeof data !== 'object') data = {};
-        for (const key in settings) {
-            if (data[key] !== settings[key]) data[key] = settings[key];
-        };
-        return data.updateOne(settings);
-    }
-
-    client.createUser = async user => {
-        const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, user);
-        const createUser = await new User(merged);
-        await createUser.save().then(u => console.log(`New user -> ${u.userName}`));
-        return false;
-    };
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
 };
