f42662d61f9d2692586da78a92e7bd4c2dbbaa6c
Zaxerone <46900584+Zaxerone@users.noreply.github.com>
2020-02-05 11:57:23 +0100

    Update Color.js

--- src/Resolver/Color.js
+++ src/Resolver/Color.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const Constants = require("./Constants");
@@ -20,25 +19,5 @@ module.exports = color => {
     throw new TypeError("Unable to convert color to a number.");
   }
   return color;
-=======
-'use strict';
 
-const Constants = require('./Constants');
-
-module.exports = (color) => {
-    if (typeof color === 'string') {
-        if (color === 'RANDOM') return Math.floor(Math.random() * (0xffffff + 1));
-        if (color === 'DEFAULT') return 0;
-        color = Constants.Colors[color] || parseInt(color.replace('#', ''), 16);
-      } else if (color instanceof Array) {
-        color = (color[0] << 16) + (color[1] << 8) + color[2];
-      }
-    
-      if (color < 0 || color > 0xffffff) {
-        throw new RangeError('Color must be within the range 0 - 16777215 (0xFFFFFF).');
-      } else if (color && isNaN(color)) {
-        throw new TypeError('Unable to convert color to a number.');
-      }
-      return color;
->>>>>>> 4c25e874e06ecebb7c351ea42ff6f8a205d2e1b1
 };
